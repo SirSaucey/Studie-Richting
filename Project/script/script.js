cb74f7b55f10b0fe29d6087294839a4ede534cbf
SirSauce <66134796+SirSaucey@users.noreply.github.com>
2023-04-18 19:09:54 +0200

    Add files via upload

--- Project/script/script.js
+++ Project/script/script.js
@@ -1,60 +1,55 @@
-// Get the options for 2e graad and 3e graad from the JSON file
 fetch('script/data.json')
-  .then(response => response.json())
-  .then(data => {
-    const options1 = data[0].options;
-    const options2 = data[0].grades[0].options;
+      .then(response => response.json())
+      .then(data => {
+        const options1 = data[0].options;
+        const options2 = data[0].grades[0].options;
 
-    // Create HTML for the options in 2e graad column
-    const options1HTML = options1.map(option => `
-    <li class="option" data-title="${option.title}">
-      <h3>${option.title}</h3>
-      <p>${option.subtitle}</p>
-    </li>
-    `).join('');
+        // Create HTML for the options in 2e graad column
+        const options1HTML = options1.map(option => `
+        <li class="option" data-title="${option.title}">
+          <h3>${option.title}</h3>
+          <p>${option.subtitle}</p>
+        </li>
+        `).join('');
 
-    // Create HTML for the options in 3e graad column
-    const options2HTML = options2.map(option => `
-      <li class="option" data-subtitle="${option.subtitle}">
-        <h3>${option.subtitle}</h3>
-        <ul>
-          ${option.choices.map(choice => `
-            <li>${choice}</li>
-          `).join('')}
-        </ul>
-      </li>
-    `).join('');
+        // Create HTML for the options in 3e graad column
+        const options2HTML = options2.map(option => `
+        <li class="option" data-title="${option.title}">
+          <h3>${option.title}</h3>
+          <p>${option.subtitle}</p>
+        </li> 
+        `).join('');
 
-    // Add the options HTML to the respective columns
-    document.getElementById('options1').innerHTML = options1HTML;
-    document.getElementById('options2').innerHTML = options2HTML;
+        // Add the options HTML to the respective columns
+        document.getElementById('options1').innerHTML = options1HTML;
+        document.getElementById('options2').innerHTML = options2HTML;
 
-    // Add hover effect to options
-    const optionElements = document.querySelectorAll('.option');
-    optionElements.forEach(option => {
-      option.addEventListener('mouseover', () => {
-        option.querySelector('h3').style.color = '#c3004a';
-        option.querySelector('p').style.color = '#c3004a';
-        option.querySelectorAll('li').forEach(li => {
-          li.style.color = '#c3004a';
-          li.style.fontWeight = 'normal';
+        // Add hover effect to options
+        const optionElements = document.querySelectorAll('.option');
+        optionElements.forEach(option => {
+          option.addEventListener('mouseover', () => {
+            option.querySelector('h3').style.color = '#c3004a';
+            option.querySelector('p').style.color = '#c3004a';
+            option.querySelectorAll('li').forEach(li => {
+              li.style.color = '#c3004a';
+              li.style.fontWeight = 'normal';
+            });
+            const borderElement = option.querySelector('.border');
+            if (borderElement) {
+              borderElement.style.border = '2px solid #c3004a';
+            }
+          });
+          option.addEventListener('mouseout', () => {
+            option.querySelector('h3').style.color = 'black';
+            option.querySelector('p').style.color = 'black';
+            option.querySelectorAll('li').forEach(li => {
+              li.style.color = 'black';
+              li.style.fontWeight = 'bold';
+            });
+            const borderElement = option.querySelector('.border');
+            if (borderElement) {
+              borderElement.style.border = 'none';
+            }
+          });
         });
-        const borderElement = option.querySelector('.border');
-        if (borderElement) {
-          borderElement.style.border = '2px solid #c3004a';
-        }
       });
-      option.addEventListener('mouseout', () => {
-        option.querySelector('h3').style.color = 'black';
-        option.querySelector('p').style.color = 'black';
-        option.querySelectorAll('li').forEach(li => {
-          li.style.color = 'black';
-          li.style.fontWeight = 'bold';
-        });
-        const borderElement = option.querySelector('.border');
-        if (borderElement) {
-          borderElement.style.border = 'none';
-        }
-      });
-    });
-  });
