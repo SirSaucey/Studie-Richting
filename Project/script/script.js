e3fcae1d481898fb40e665530db910a2c99eacd0
SirSauce <66134796+SirSaucey@users.noreply.github.com>
2023-04-26 10:56:19 +0200

    Add files via upload

--- Project/script/script.js
+++ Project/script/script.js
@@ -7,18 +7,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
       // Create HTML for the options in 2e graad column
       const options1HTML = options1.map(option => `
-      <li class="option" data-title="${option.title}">
-        <h3>${option.title}</h3>
-        <p>${option.subtitle}</p>
-      </li>
+        <li class="option ${option.className}" data-title="${option.title}">
+          <h3>${option.title}</h3>
+          <p>${option.subtitle}</p>
+        </li>
       `).join('');
 
       // Create HTML for the options in 3e graad column
       const options2HTML = options2.map(option => `
-      <li class="option" data-title="${option.title}">
-        <h3>${option.title}</h3>
-        <p>${option.subtitle}</p>
-      </li> 
+        <li class="option ${option.className}" data-title="${option.title}">
+          <h3>${option.title}</h3>
+          <p>${option.subtitle}</p>
+        </li> 
       `).join('');
 
       // Add the options HTML to the respective columns
@@ -26,62 +26,21 @@ document.addEventListener('DOMContentLoaded', () => {
       document.getElementById('options2').innerHTML = options2HTML;
 
       // Add hover effect to options
-      // Add hover effect to options
-    const optionElements = document.querySelectorAll('.option');
-    optionElements.forEach(option => {
-      option.addEventListener('mouseover', () => {
-      option.querySelector('h3').style.color = '#c3004a';
-      option.querySelectorAll('li').forEach(li => {
-        li.style.color = '#c3004a';
-        li.style.fontWeight = 'normal';
-      });
-      const borderElement = option.querySelector('.border');
-      if (borderElement) {
-        borderElement.style.border = '2px solid #c3004a';
-      }
+      document.querySelectorAll('.option').forEach(option => {
+        option.addEventListener('mouseover', () => {
+          // Change color of all elements with the same class
+          const className = option.getAttribute('class').split(' ')[1];
+          document.querySelectorAll(`.${className}`).forEach(el => {
+            el.style.color = '#c3004a'; // change the color to #c3004a
+          });
+        });
 
-    // Find related options and update their colors
-    const relatedOptions = option.dataset.related ? document.querySelectorAll(`[data-related="${option.dataset.related}"]`) : [];
-    relatedOptions.forEach(relatedOption => {
-      relatedOption.querySelector('h3').style.color = '#c3004a';
-      relatedOption.querySelector('p').style.color = '#c3004a';
-      relatedOption.querySelectorAll('li').forEach(li => {
-        li.style.color = '#c3004a';
-        li.style.fontWeight = 'normal';
-      });
-      const relatedBorderElement = relatedOption.querySelector('.border');
-      if (relatedBorderElement) {
-        relatedBorderElement.style.border = '2px solid #c3004a';
-      }
-    });
-  });
-
-  option.addEventListener('mouseout', () => {
-    option.querySelector('h3').style.color = 'black';
-    option.querySelector('p').style.color = 'black';
-    option.querySelectorAll('li').forEach(li => {
-      li.style.color = 'black';
-      li.style.fontWeight = 'bold';
-    });
-    const borderElement = option.querySelector('.border');
-    if (borderElement) {
-      borderElement.style.border = 'none';
-    }
-
-    // Find related options and update their colors
-    const relatedOptions = option.dataset.related ? document.querySelectorAll(`[data-related="${option.dataset.related}"]`) : [];
-    relatedOptions.forEach(relatedOption => {
-      relatedOption.querySelector('h3').style.color = 'black';
-      relatedOption.querySelectorAll('li').forEach(li => {
-        li.style.color = 'black';
-        li.style.fontWeight = 'bold';
-      });
-      const relatedBorderElement = relatedOption.querySelector('.border');
-      if (relatedBorderElement) {
-        relatedBorderElement.style.border = 'none';
-          }
+        option.addEventListener('mouseout', () => {
+          // Reset color of all elements with a class
+          document.querySelectorAll('[class]').forEach(el => {
+            el.style.color = ''; // reset the color to its original value
+          });
         });
       });
     });
-  });
 });
